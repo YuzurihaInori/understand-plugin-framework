c83fc28a2a30708d33fc080815c83cd89882af49
YuzurihaInori <823130614@qq.com>
2016-06-22 15:18:49 +0800

    添加activity.startActivity的hook

--- dynamic-proxy-hook/src/main/java/com/weishu/upf/dynamic_proxy_hook/app2/MainActivity.java
+++ dynamic-proxy-hook/src/main/java/com/weishu/upf/dynamic_proxy_hook/app2/MainActivity.java
@@ -14,14 +14,17 @@ import com.weishu.upf.dynamic_proxy_hook.app2.hook.HookHelper;
  * @author weishu
  * @date 16/1/28
  */
-public class MainActivity extends Activity {
+public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // TODO: 16/1/28 支持Activity直接跳转请在这里Hook
-        // 家庭作业,留给读者完成.
-
+        try {
+            // 在这里进行Hook
+            HookHelper.attachActivity(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         Button tv = new Button(this);
         tv.setText("测试界面");
 
@@ -38,7 +41,7 @@ public class MainActivity extends Activity {
                 // 因为Activity对象的startActivity使用的并不是ContextImpl的mInstrumentation
                 // 而是自己的mInstrumentation, 如果你需要这样, 可以自己Hook
                 // 比较简单, 直接替换这个Activity的此字段即可.
-                getApplicationContext().startActivity(intent);
+                startActivity(intent);
             }
         });
     }
--- dynamic-proxy-hook/src/main/java/com/weishu/upf/dynamic_proxy_hook/app2/hook/HookHelper.java
+++ dynamic-proxy-hook/src/main/java/com/weishu/upf/dynamic_proxy_hook/app2/hook/HookHelper.java
@@ -1,9 +1,12 @@
 package com.weishu.upf.dynamic_proxy_hook.app2.hook;
 
+import android.app.Activity;
 import android.app.Instrumentation;
+import android.util.Log;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Calendar;
 
 /**
  * @author weishu
@@ -11,7 +14,7 @@ import java.lang.reflect.Method;
  */
 public class HookHelper {
 
-    public static void attachContext() throws Exception{
+    public static void attachContext() throws Exception {
         // 先获取到当前的ActivityThread对象
         Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
         Method currentActivityThreadMethod = activityThreadClass.getDeclaredMethod("currentActivityThread");
@@ -28,5 +31,37 @@ public class HookHelper {
 
         // 偷梁换柱
         mInstrumentationField.set(currentActivityThread, evilInstrumentation);
+    }
+
+    public static void attachActivity(Object object) throws Exception {
+//        // 先获取到当前的ActivityThread对象
+//        Class<?> activityClass = Class.forName("android.app.Activity");
+//        Method getApplication = activityClass.getDeclaredMethod("getApplication");
+//        getApplication.setAccessible(true);
+//        Object application = getApplication.invoke(null);
+
+        Class<?> clazz = object.getClass();
+
+        while (clazz != Activity.class) {
+            clazz = clazz.getSuperclass();
+        }
+
+        Log.e("====", clazz.getName());
+
+
+        // 拿到原始的 mInstrumentation字段
+
+        Field mInstrumentationField = clazz.getDeclaredField("mInstrumentation");
+
+        mInstrumentationField.setAccessible(true);
+        Instrumentation mInstrumentation = (Instrumentation) mInstrumentationField.get(object);
+
+        // 创建代理对象
+        Instrumentation evilInstrumentation = new EvilInstrumentation(mInstrumentation);
+
+        // 偷梁换柱
+        mInstrumentationField.set(object, evilInstrumentation);
+
+
     }
 }
